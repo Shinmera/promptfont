7bd6ae84fbda66365d28ecfd92de59f046e41fcb
Shinmera <shinmera@tymoon.eu>
2023-03-19 15:21:17 +0100

    Fixup on compile

--- compile.lisp
+++ compile.lisp
@@ -2,7 +2,7 @@
 sbcl --noinform --load "$0" --eval '(generate)' --quit; exit
 |#
 
-(ql:quickload '(clip yason) :silent T)
+(ql:quickload '(clip shasht) :silent T)
 
 (defvar *here* #.(or *compile-file-pathname*
                      *load-pathname*
@@ -13,17 +13,20 @@ sbcl --noinform --load "$0" --eval '(generate)' --quit; exit
 
 (defun parse-glyphs (&optional (file (file "glyphs" "json")))
   (let ((sections (make-hash-table :test 'equal)))
-    (dolist (glyph (yason:parse file))
-      (push (list :code (gethash "code" glyph)
-                  :codepoint (gethash "codepoint" glyph)
-                  :name (gethash "name" glyph))
-            (gethash (gethash "category" glyph) sections)))
+    (loop for glyph across (with-open-file (stream file)
+                             (shasht:read-json stream))
+          do (push (list :code (gethash "code" glyph)
+                         :codepoint (gethash "codepoint" glyph)
+                         :name (gethash "name" glyph))
+                   (gethash (gethash "category" glyph) sections)))
     (loop for name being the hash-keys of sections
           for glyphs being the hash-values of sections
           collect (list :name name
                         :glyphs (sort glyphs #'< :key (lambda (a) (getf a :codepoint)))))))
 
 (defun generate (&key (input (file "index" "ctml")) (output (file "index" "html")))
+  (load (file "fixup" "lisp"))
+  (fixup)
   (let ((sections (parse-glyphs)))
     (with-open-file (stream output :direction :output :if-exists :supersede)
       (plump:serialize (clip:process input :sections sections) stream))))
